a90814e73cc5b1f9b2b86478e0cf6a608db659b1
JackDarkWeb <jacknouatin@yahoo.fr>
2018-11-11 15:39:48 +0200

    add route

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    function show($page){
+
+        return view('pages.'.$page);
+    }
+}
