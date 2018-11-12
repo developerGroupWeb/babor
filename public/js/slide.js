7df408304af5cde5cdbc9d4c3c72f7b9e617bf3c
JackDarkWeb <jacknouatin@yahoo.fr>
2018-11-12 10:25:51 +0200

    test slider

---
+++ public/js/slide.js
@@ -0,0 +1,37 @@
+$(function(){
+
+    var slider         = $('#slider');
+    var slideContainer = slider.find('.slides');
+    var slides         = slideContainer.find('.slide');
+
+    var currentSlide = 1;
+    var width = 720;
+
+    var interval;
+
+    function startSlider(){
+
+        setInterval(function(){
+
+            slideContainer.animate({
+
+                    marginLeft: '-='+width
+
+                }, 1000,
+                function () {
+                    currentSlide++;
+                    if (currentSlide === slides.length){
+                        currentSlide = 1;
+                        slideContainer.css('margin-left', 0);
+                    }
+                }
+            );
+
+        }, 3000);
+    }
+
+    startSlider();
+
+
+
+});
---
+++ resources/views/pages/slide.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <style>
+
+        #slider{
+            width: 720px;
+            height: 400px;
+            overflow: hidden;
+        }
+        .slides{
+            display: block;
+            width: 6000px;
+            height: 400px;
+            padding: 0;
+            margin: 0;
+        }
+
+
+        .slide img{
+            list-style-type: none;
+            width: 720px;
+            height: 400px;
+            float: left;
+        }
+
+    </style>
+
+<div id="slider">
+
+    <ul class="slides">
+
+        <li class="slide"><img src="{{asset('images/imag.jpg')}}"/></li>
+        <li class="slide"><img src="{{asset('images/imfine.jpg')}}"/></li>
+        <li class="slide"><img src="{{asset('images/rencontrer-femme-africaine-black-celibataire.jpg')}}"/></li>
+        <li class="slide"><img src="{{asset('images/imag.jpg')}}"/></li>
+
+    </ul>
+
+</div>
+
+@stop
+
